8f75a3d75709029aff5023003d958477a1353fc1
satriayp262 <127200227+satriayp262@users.noreply.github.com>
2023-11-16 20:37:30 +0700

    Add files via upload

--- penjadwalan.sql
+++ penjadwalan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Nov 16, 2023 at 01:32 PM
+-- Generation Time: Nov 16, 2023 at 01:36 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
