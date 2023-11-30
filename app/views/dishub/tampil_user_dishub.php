a07e24de87340341caeae738c2cd3d78412fc6d7
satriayp262 <127200227+satriayp262@users.noreply.github.com>
2023-11-30 15:00:40 +0700

    progress

--- app/views/dishub/tampil_user_dishub.php
+++ app/views/dishub/tampil_user_dishub.php
@@ -204,7 +204,7 @@ $db = new Database();
         <div class="px-5 py-2">
             <h3>DAFTAR USERNAME ANGGOTA</h3>
             <div>
-                <a href="tambah_pnp_dishub.php" class="btn btn-primary mb-3 float-start"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square-fill" viewBox="0 0 16 16">
+                <a href="tambah_user_dishub.php" class="btn btn-primary mb-3 float-start"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square-fill" viewBox="0 0 16 16">
                         <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3a.5.5 0 0 1 1 0z" />
                     </svg> Tambah Data User</a>
             </div>
